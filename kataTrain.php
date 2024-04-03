f72143ab8808e8c228e2556de7e5cc48c79f3455
Reivax9696 <149475827+Reivax9696@users.noreply.github.com>
2024-04-03 13:41:24 +0200

    commit2

--- kataTrain.php
+++ kataTrain.php
@@ -4,48 +4,17 @@ function calculateSum($array) {
     return array_sum($array);
 }
 
-$partialSum = 0;
-$enteredNumbers = [];
-$numberCount = 0;
-$firstCountSuccess = false;
-
-while ($partialSum != 67 && $numberCount < 10) {
-    echo "Escriu un numero (1-9): ";
-    $number = intval(trim(fgets(STDIN)));
-
-    if ($number < 1 || $number > 9) {
-        echo "Número invalid. Introdueix un número entre el 1 i el 9.\n";
-        continue;
-    }
-
-    $enteredNumbers[] = $number;
-    $numberCount++;
-
-    $partialSum = calculateSum($enteredNumbers);
-
-    echo "Suma Parcial: $partialSum\n";
-
-    if ($partialSum == 67) {
-        echo "Primer fre d'emergència activat!\n";
-        $firstCountSuccess = true;
-        break; 
-    } elseif ($numberCount >= 10) {
-        echo "El tren explota! Game Over!";
-        exit; 
-    }
-}
-
-if ($firstCountSuccess) {
+function numberCounter($targetSum, $showPartialSum = true){
     $partialSum = 0;
     $enteredNumbers = [];
     $numberCount = 0;
 
-    while ($partialSum != 81 && $numberCount < 10) {
-        echo "Escriu un numero (1-9): ";
+    while ($partialSum != $targetSum && $numberCount < 10) {
+        echo "Escriu un número (1-9): ";
         $number = intval(trim(fgets(STDIN)));
 
         if ($number < 1 || $number > 9) {
-            echo "Número Invalid. Introdueix un número entre el 1 i el 9.\n";
+            echo "Número invàlid. Introdueix un número entre 1 i 9.\n";
             continue;
         }
 
@@ -53,11 +22,30 @@ if ($firstCountSuccess) {
         $numberCount++;
 
         $partialSum = calculateSum($enteredNumbers);
+
+        if ($showPartialSum) {
+            echo "Suma Parcial: $partialSum\n";
+        }
     }
 
-    if ($partialSum == 81) {
+    return $partialSum;
+
+}
+
+
+$firstCount = numberCounter(67);
+
+if ($firstCount == 67) {
+
+    echo ("Primer fre d'emergència activat! Pero hi ha un altre bomba!\n");
+
+    $secondCount = numberCounter(81, false);
+
+    if ($secondCount == 81) {
         echo "Segon fre d'emergència activat! Has salvat el tren!";
     } else {
         echo "El tren explota! Game Over!";
     }
+} else {
+    echo "El tren explota! Game Over!";
 }
